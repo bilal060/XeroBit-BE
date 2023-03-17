a161ae6bb9ca20b6c6866ce0238f7873607245d2
Apple <apple@Apples-MacBook-Pro.local>
2023-03-17 02:39:11 -0700

    Added comment

--- src/middleware/auth.ts
+++ src/middleware/auth.ts
@@ -8,7 +8,7 @@ import { USER_TYPE } from '../constants'
 
 export async function isAuthorized(req: Request, res: Response, next: () => void) {
   console.log("is Autherized")
-
+  // comment added
   if (req.headers.authorization) {
     const token = req.headers.authorization.split(' ')[1];
     try {
