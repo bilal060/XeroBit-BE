3f557775d0cf6423324d31d7c2f4345db920c681
Muhammad Numan <numanrasheed316@gmail.com>
2023-04-17 21:41:09 +0500

    Add Contact us changes

--- src/models/services.ts
+++ src/models/services.ts
@@ -1,64 +1,64 @@
 import {
-    Model, Schema, model
+  Model, Schema, model
 } from 'mongoose';
 import TimeStampPlugin, {
-    ITimeStampedDocument
+  ITimeStampedDocument
 } from './plugins/timestamp-plugin';
 
 interface ISection {
-    sectionTitle: string;
-    sectionDescription: string;
-    sectionImage: {
-      data: Buffer;
-      contentType: string;
-    };
-  }
+  sectionTitle: string;
+  sectionDescription: string;
+  sectionImage: {
+    data: Buffer;
+    contentType: string;
+  };
+}
 export interface Iservices extends ITimeStampedDocument {
-    /** Name of the BLog Title */
-    serviceTitle: string;
-    // service Category
-    serviceCategory: string
-    // service Description
-    description: string
-    // source
-    source: string
-    // link
-    links: [{ Site: string, src: string }],
-    // service Image
-    serviceImage: {
-        data: Buffer,
-        contentType: String
-    }
-    // serviceImage: [{ images: Buffer }]
-    sections: ISection[];
+  /** Name of the BLog Title */
+  serviceTitle: string;
+  // service Category
+  serviceCategory: string
+  // service Description
+  description: string
+  // source
+  source: string
+  // link
+  links: [{ Site: string, src: string }],
+  // service Image
+  serviceImage: {
+    data: Buffer,
+    contentType: String
+  }
+  // serviceImage: [{ images: Buffer }]
+  sections: ISection[];
 
 }
 
 interface IservicesModel extends Model<Iservices> { }
 const sectionSchema = new Schema<ISection>({
-    sectionTitle: { type: String, required: true },
-    sectionDescription: { type: String, required: true },
-    sectionImage: {
-      type: {
-        data: Buffer,
-        contentType: String
-      },
-      required: true
-    }
-  });
-const schema = new Schema<Iservices>({
-    serviceTitle: { type: String, required: true },
-    serviceCategory: { type: String },
-    description: { type: String, required: true },
-    source: { type: String },
-    links: { type: Array<Object>, default: [] },
-    serviceImage: {
-        type: {
-            data: Buffer,
-            contentType: String
-        }, required: true
+  sectionTitle: { type: String, required: true },
+  sectionDescription: { type: String, required: true },
+  sectionImage: {
+    type: {
+      data: Buffer,
+      contentType: String
     },
-    sections: { type: [sectionSchema], default: [] }
+    required: true
+  }
+});
+const schema = new Schema<Iservices>({
+  serviceTitle: { type: String, required: true },
+  serviceCategory: { type: String },
+  description: { type: String, required: true },
+  source: { type: String },
+  links: { type: Array<Object>, default: [] },
+  serviceImage: {
+    type: {
+      data: Buffer,
+      contentType: String
+    }, required: true
+  },
+  sections: { type: [sectionSchema], default: [] }
 });
 
 // Add timestamp plugin for createdAt and updatedAt in miliseconds from epoch
